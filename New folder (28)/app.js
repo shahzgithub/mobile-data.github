331dea239f1dd49c2ea80e0d19fda59ba575d412
shahzgithub <143381478+shahzgithub@users.noreply.github.com>
2023-12-19 23:33:25 +0500

    Add files via upload

---
+++ New folder (28)/app.js
@@ -0,0 +1,245 @@
+let arr = [
+    {
+        brand: "Samsung",
+        model: "SamsungA30",
+        price: "40000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA10",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA20",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY20",
+        price: "20000",
+        camera: "40px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY11",
+        price: "20000",
+        camera: "40px",
+        ram: "3gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY15",
+        price: "30000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "64gb",
+        
+    },
+    {
+        brand: "Motorola",
+        model: "Motorola123",
+        price: "5000",
+        camera: "2px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone12",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone13",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "IphoneX",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+]
+
+
+const samsung = [
+    {
+        brand: "Samsung",
+        model: "SamsungA30",
+        price: "40000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA10",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Samsung",
+        model: "SamsungA20",
+        price: "40000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "128gb",
+        
+    },
+]
+
+console.log(samsung)
+const Vivo = [
+    {
+        brand: "Vivo",
+        model: "VivoY20",
+        price: "20000",
+        camera: "40px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY11",
+        price: "20000",
+        camera: "40px",
+        ram: "3gb",
+        rom : "32gb",
+        
+    },
+    {
+        brand: "Vivo",
+        model: "VivoY15",
+        price: "30000",
+        camera: "40px",
+        ram: "4gb",
+        rom : "64gb",
+        
+    },
+]
+const Motorola = [
+    {
+        brand: "Motorola",
+        model: "Motorola123",
+        price: "5000",
+        camera: "2px",
+        ram: "2gb",
+        rom : "32gb",
+        
+    },
+]
+const Iphone = [
+    {
+        brand: "Iphone",
+        model: "Iphone12",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "Iphone13",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    {
+        brand: "Iphone",
+        model: "IphoneX",
+        price: "300000",
+        camera: "40px",
+        ram: "8gb",
+        rom : "128gb",
+        
+    },
+    
+]
+
+const render_mobile_data = document.getElementById("mobile_data");
+samsung.map((current_object, current_index) => {
+    const {brand, model, price, camera, ram, rom} = current_object;
+
+
+    return (render_mobile_data.innerHTML += `<ul>
+    <li>Brand: ${samsung} </li>
+    <li>Model: ${amsungA30}</li>
+    <li>Price: ${40000}</li>
+    <li>Camera: ${mobile_camera}</li>
+    <li>Ram: ${mobile_ram}</li>
+    <li>Rom: ${mobile_rom}</li>
+</ul>
+
+<ul>
+    <li>Brand: ${samsung}</li>
+    <li>Model: ${amsungA10}</li>
+    <li>Price: ${40000}</li>
+    <li>Camera: ${mobile_camera}</li>
+    <li>Ram: ${mobile_ram}</li>
+    <li>Rom: ${mobile_rom}</li>
+</ul>
+
+<ul>
+    <li>Brand: ${samsung}</li>
+    <li>Model: ${amsungA20}</li>
+    <li>Price: ${40000}</li>
+    <li>Camera: ${mobile_camera}</li>
+    <li>Ram: ${mobile_ram}</li>
+    <li>Rom: ${mobile_rom}</li>
+</ul>`);
+});
+
+console.log(arr1)
+
+function search() {
+    const inputvalue = document.getElementById(searchValue);
+    console.log(samsung, Vivo, Motorola, Iphone);
+
+};
+
+search();
+
+
+
+
+
+
+
